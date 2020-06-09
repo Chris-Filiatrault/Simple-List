007131170d049a9bd49ae77544aaa40088b3690a
Chris Filiatrault <chris Filiatrault@hotmail.com>
2020-06-10 08:52:05 +1000

    Initial Commit

---
+++ Simple ListTests/Simple_ListTests.swift
@@ -0,0 +1,34 @@
+//
+//  Simple_ListTests.swift
+//  Simple ListTests
+//
+//  Created by Chris Filiatrault on 10/6/20.
+//  Copyright © 2020 Chris Filiatrault. All rights reserved.
+//
+
+import XCTest
+@testable import Simple_List
+
+class Simple_ListTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
