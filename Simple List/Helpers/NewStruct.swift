bbb0d22dff964a0893e8beebf0dae0522e6f6ea0
Chris Filiatrault <chris Filiatrault@hotmail.com>
2020-06-19 19:42:04 +1000

    Test upload with NewStruct

---
+++ Simple List/Helpers/NewStruct.swift
@@ -0,0 +1,21 @@
+//
+//  NewStruct.swift
+//  Simple List
+//
+//  Created by Chris Filiatrault on 19/6/20.
+//  Copyright © 2020 Chris Filiatrault. All rights reserved.
+//
+
+import SwiftUI
+
+struct NewStruct: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NewStruct_Previews: PreviewProvider {
+    static var previews: some View {
+        NewStruct()
+    }
+}
