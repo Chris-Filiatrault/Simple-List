f2cf6c48c1973f8a94e9a54ca6191f8607e78d58
Chris Filiatrault <chris Filiatrault@hotmail.com>
2020-06-27 10:53:39 +1000

    1.3.5 - set "Application requires iPhone environment" to YES, hoping it will allow the app to show on the iPad store...

--- ToDo.swift
+++ ToDo.swift
@@ -5,38 +5,45 @@
  ====== Before next release ======
  ---------------------------------
  
- Version in progress: 1.3.4
+ Version in progress: 1.3.5
+  
+ - fix Japanese input issue
  
+ - Fix the item name not displaying properly when editing the name, when the existing item name is long (multiline + smaller font)
+ 
+ - If setting "Application requires iPhone environment" to YES doesn't allow iPad app to show up on the store, then create two targets, one for iPad and one for iPhone
  
  ---
  
  - Make sure version no. in project info is correct
  
+ - Commit, then push to Development & Production on GitHub
+ 
+ 
+ 
+ 
+ 
+ 
  
 
  ---------------------------------
- =========== TODO ============
+ ============= TODO ==============
  ---------------------------------
  
- - Fix the item name not displaying properly when editing the name, when the existing item name is long (multiline + smaller font)
+
  
+ 
+ 
+ 
+ ----------------------------------
+ ======== Things for later ========
+ ----------------------------------
+ 
+ - Japanese version (by changing strings)
+
  - Brighten the photo in light mode
  
- 
- 
- 
- 
- 
- 
- 
- 
- ----------------------------------
- ===== Things for later =====
- ----------------------------------
-
  - Mac version
- 
- - Japanese version
  
  - Increase active area textfields
  
